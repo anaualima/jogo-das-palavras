173a7e0495f567ebc778cae77b72019cc50c5a3b
Alissandra <alissandraraysa@gmail.com>
2022-07-27 10:50:42 -0300

    criacao de tela inicial do jogo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Telainicial from './components/Telainicial';
 
 function App() {
   return (
     <div className="App">
-      <h2>Jogo de Palavras</h2>
+      <Telainicial />
     </div>
   );
 }
---
+++ src/components/Telainicial.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Telainicial.css";
+
+const Telainicial = () => {
+  return (
+    <div className="start">
+      <h1>Jogo de Palavras</h1>
+      <p>Clique aqui para começar a jogar</p>
+      <button>Iniciar o jogo</button>
+    </div>
+  )
+}
+
+export default Telainicial;
