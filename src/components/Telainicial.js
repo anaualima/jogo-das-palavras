cfeb142b86078be87f7cce51ce76ca9fb5e4e44e
Alissandra <alissandraraysa@gmail.com>
2022-07-28 19:54:14 -0300

    estilizacao final

--- src/components/Telainicial.js
+++ src/components/Telainicial.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Telainicial.css';
-import Capa from '../images/capa.png';
+// import Capa from '../images/capa.png';
+// import Capa from '../components/capa.gif';
+import Capa from '../images/capa.svg'
 
 const Telainicial = ({ iniciarJogo }) => {
   return (
     <div className="start">
       <div className="start-square">
-        <img src={Capa} alt="capa-do-jogo" width="600" />
+        <h1>Jogo de Palavras</h1>
+        <img src={Capa} alt="capa-do-jogo" />
         <p>Clique aqui para começar a jogar</p>
         <button onClick={iniciarJogo}>Iniciar o jogo</button>
       </div>
